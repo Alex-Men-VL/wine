78af1b9961f9d3f9b934541410693374ffe6e6d5
Aleksandr <menshikov.a.v@icloud.com>
2021-08-06 20:12:16 +0300

    (Step 11) Add cycle according to data from wine.xlsx

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from datetime import datetime
 
+import pandas
+
 
 env = Environment(
     loader=FileSystemLoader('.'),  
@@ -12,9 +14,13 @@ env = Environment(
 
 template = env.get_template('template.html')
 
+excel_data_df = pandas.read_excel('wine.xlsx')
+
+wines = excel_data_df.to_dict('records')
 
 rendered_page = template.render(
     winery_age = datetime.now().year - 1920,
+	wines = wines,
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
