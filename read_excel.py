226a9bfef861d7b633efcd6235052be5257ba08c
Aleksandr <menshikov.a.v@icloud.com>
2021-08-06 22:18:58 +0300

    (Step 12) add wine2.xlsx file and download data from it in the correct form

--- read_excel.py
+++ read_excel.py
@@ -1,6 +1,20 @@
 import pandas
 
 
-excel_data_df = pandas.read_excel('wine.xlsx')
+from pprint import pprint
 
-print(excel_data_df.to_dict('records'))
+
+excel_data_df = pandas.read_excel(
+    'wine2.xlsx', na_values=' ', keep_default_na=False)
+
+data = excel_data_df.to_dict('records')
+
+categories = set(excel_data_df['Категория'].tolist())
+
+list_of_drinks = {}
+for category in categories:
+	list_of_drinks[category] = [drink for drink in data if drink['Категория'] == category]
+
+
+
+pprint(list_of_drinks)
