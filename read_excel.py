1d667d2e8035db8a41b622ec6fb0a1e19f0a3e02
Aleksandr <menshikov.a.v@icloud.com>
2021-08-06 22:39:00 +0300

    (Step 13) group products from wine2.xlsx other way

--- read_excel.py
+++ read_excel.py
@@ -1,20 +1,24 @@
 import pandas
 
-
 from pprint import pprint
+
+import collections
 
 
 excel_data_df = pandas.read_excel(
     'wine2.xlsx', na_values=' ', keep_default_na=False)
 
-data = excel_data_df.to_dict('records')
+drinks = excel_data_df.to_dict('records')
 
 categories = set(excel_data_df['Категория'].tolist())
 
-list_of_drinks = {}
-for category in categories:
-	list_of_drinks[category] = [drink for drink in data if drink['Категория'] == category]
+# list_of_drinks = {}
+# for category in categories:
+# 	list_of_drinks[category] = [drink for drink in drinks if drink['Категория'] == category]
 
+list_of_drinks = collections.defaultdict(list)
 
+for drink  in drinks:
+	list_of_drinks[drink['Категория']].append(drink)
 
 pprint(list_of_drinks)
