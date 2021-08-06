732d0cc1d6ccba701a33fa0e549d969f83ac4d81
Aleksandr <menshikov.a.v@icloud.com>
2021-08-07 01:25:35 +0300

    remove the commented out code

--- main.py
+++ main.py
@@ -22,10 +22,6 @@ excel_data_df = pandas.read_excel(
     'wine3.xlsx', na_values=' ', keep_default_na=False)
 drinks = excel_data_df.to_dict('records')
 
-# list_of_drinks = collections.defaultdict(list)
-# for drink in drinks:
-# 	list_of_drinks[drink['Категория']].append(drink)
-
 categories = list(set(excel_data_df['Категория'].tolist()))
 categories.sort()
 
